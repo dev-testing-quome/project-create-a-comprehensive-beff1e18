cc63b7357fbd434b3c73430b6fbda39afa47c0ff
jim-schwoebel <jim-schwoebel@users.noreply.github.com>
2025-07-24 19:06:22 +0000

    🎨 Add frontend code and user interface

---
+++ frontend/src/pages/PermitStatusPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PermitStatusPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Permit Status</h1>
+      {/* Add permit status display here */}
+    </div>
+  );
+};
+
+export default PermitStatusPage;
